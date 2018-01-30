d5b2919dbd5a4c1b1aebce926c818c569b4f6615
周佳奇 <zhoujiaqi@hozonauto.com>
2018-01-30 18:50:28 +0800

    Add vollery library

--- sample/src/main/java/com/aircore/LinqiApplication.java
+++ sample/src/main/java/com/aircore/LinqiApplication.java
@@ -3,16 +3,21 @@ package com.aircore;
 import android.app.Application;
 import android.content.Context;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 import cube.com.axislibrary.factory.Axis;
 
 public class LinqiApplication extends Application {
     private static LinqiApplication instance;
+    private static RequestQueue queues ;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
         Axis.init(this,1080,1920);//适配初始化,基准宽高
+        queues = Volley.newRequestQueue(getApplicationContext());
     }
 
     /**
@@ -26,4 +31,7 @@ public class LinqiApplication extends Application {
         return instance;
     }
 
+    public static RequestQueue getHttpQueues() {
+        return queues;
+    }
 }
--- sample/src/main/java/com/aircore/MainActivity.java
+++ sample/src/main/java/com/aircore/MainActivity.java
@@ -2,9 +2,15 @@ package com.aircore;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.aircore.util.BaseUtils;
 import com.aircore.view.ControlView;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +23,27 @@ public class MainActivity extends AppCompatActivity {
         BaseUtils.setWindowStatusBarColor(this);
         view = new ControlView(this);
         setContentView(view);
+
+
+        String URL = "https://www.ecarobo.com/api/userservice/mobileaccount/getappversion?version=7&appId=102190655212731";
+        StringRequest request = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {//s为请求返回的字符串数据
+                        Log.e("1111",s);
+                        Toast.makeText(MainActivity.this,s+"",Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.e("aaaaa",volleyError.toString()+"");
+                    }
+                });
+        //设置请求的Tag标签，可以在全局请求队列中通过Tag标签进行请求的查找
+        request.setTag("testGet");
+        //将请求加入全局队列中
+        LinqiApplication.getHttpQueues().add(request);
     }
 
 }
